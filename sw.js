2ced926b402cefd915f4964d98898a353e9b1e92
Andre Gonzales <gibstock@gmail.com>
2022-03-18 10:18:07 -0700

    adding tests for soundfile from google drive

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'site-static-v23-19' // need to update version with changes
-const dynamicCacheName = 'site-dynamic-v6-07' // need to update version with changes
+const cacheName = 'site-static-v23-20' // need to update version with changes
+const dynamicCacheName = 'site-dynamic-v6-08' // need to update version with changes
 const assets = [
   '/',
   '/index.html',
